72b52d622b30a7aff8414c8382b24bec88b893f2
Matt Moore <mattmoore@pop-os.localdomain>
2023-04-22 20:55:24 -0400

    Challenge 5 complete

---
+++ Prototype5/Assets/Challenge 5/Scripts/DifficultyButtonX.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButtonX : MonoBehaviour
+{
+    private Button button;
+    private GameManagerX gameManagerX;
+    public int difficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManagerX = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    /* When a button is clicked, call the StartGame() method
+     * and pass it the difficulty value (1, 2, 3) from the button 
+    */
+    void SetDifficulty()
+    {
+        int difficulty = 1;
+        switch (button.gameObject.name)
+        {
+            case ("Medium Button"):
+                difficulty = 2;
+                break;
+            case ("High Button"):
+                difficulty = 3;
+                break;
+        }
+        gameManagerX.StartGame(difficulty);
+    }
+
+
+
+}
